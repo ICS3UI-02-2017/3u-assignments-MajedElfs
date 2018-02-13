9cae87bd137ba86888cf7914de8eda379051127c
hadim9637 <hadim9637@HRH-IC0027346.wrdsb.ca>
2018-02-13 11:06:09 -0500

    Finished the rest of the questions.

--- src/Assignment1/A1Q3.java
+++ src/Assignment1/A1Q3.java
@@ -7,6 +7,8 @@ package Assignment1;
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.RobotSE;
+import becker.robots.Thing;
+import becker.robots.Wall;
 
 /**
  *
@@ -22,7 +24,44 @@ public class A1Q3 {
          //Making a city
        City moongale = new City();
        
-       //Making a robot start at its "bed"
-       RobotSE elf = new RobotSE (moongale,1,2,Direction.SOUTH);
+       //Making a robot, and making it start at the bottom of the hill
+       RobotSE elf = new RobotSE (moongale,3,0,Direction.EAST);
+       
+       //Making the flag (thing) start at the bottom of the hill
+       Thing flag = new Thing (moongale,3,1);
+       
+       
+       //Making the "hill" using walls
+       new Wall (moongale,3,2,Direction.WEST);
+       new Wall (moongale,3,2,Direction.NORTH);
+       new Wall (moongale,2,3,Direction.WEST);
+       new Wall (moongale,1,3,Direction.WEST);
+       new Wall (moongale,1,3,Direction.NORTH);
+       new Wall (moongale,1,3,Direction.EAST);
+       new Wall (moongale,2,4,Direction.NORTH);
+       new Wall (moongale,2,4,Direction.EAST);
+       new Wall (moongale,3,4,Direction.EAST);
+       
+       //Making robot pick up thing (the flag) and climb up the hill
+       elf.move();
+       elf.pickThing();
+       elf.turnLeft();
+       elf.move();
+       elf.turnRight();
+       elf.move();
+       elf.turnLeft();
+       elf.move(2);
+       elf.turnRight();
+       elf.move();
+       //Making robot drop the thing (the flag) and go down the hill
+       elf.putThing();
+       elf.move();
+       elf.turnRight();
+       elf.move();
+       elf.turnLeft();
+       elf.move();
+       elf.turnRight();
+       elf.move(2);
+       elf.turnLeft();
     }
 }
