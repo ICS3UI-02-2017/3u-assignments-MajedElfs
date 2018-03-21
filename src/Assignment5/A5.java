333e110b4f199a89f39e7def6ca00ebdbe2bbfc0
hadim9637 <hadim9637@HRH-IC0027346.wrdsb.ca>
2018-03-21 11:17:48 -0400

    Still working on the Ubbi Dubbi translator, qutie hard.

--- src/Assignment5/A5.java
+++ src/Assignment5/A5.java
@@ -17,45 +17,48 @@ public class A5 {
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         //Repeat translation
-        while(true){
+        while (true) {
             //Getting the word
             System.out.println("Enter a word to UBBIFY!");
             String origWord = input.nextLine();
             //Sanitize the input
             origWord = origWord.toLowerCase();
-            
+
             //Checking if the word starts with a vowel
-            if (origWord.startsWith("a")||origWord.startsWith("i")||origWord.startsWith("e")||origWord.startsWith("o")||origWord.startsWith("u")){
+            if (origWord.startsWith("a") || origWord.startsWith("i") || origWord.startsWith("e") || origWord.startsWith("o") || origWord.startsWith("u")) {
                 //Put "ub" in front of word
                 String transWord = "Ub" + origWord;
-                System.out.println("Original word: "+origWord +". Translated word: "+transWord+".");
-            }else{
+                System.out.println("Original word: " + origWord + ". Translated word: " + transWord + ".");
+            } else {
                 //Walk down the word looking for a vowel
                 int length = origWord.length();
                 //Use a for loop to go through all the letters
-                for(int i = 0; i < length; i++){
+                for (int i = 0; i < length; i++) {
                     //Look at the character at position 'i' and is it a vowel?
-                    if (origWord.charAt(i)== 'a' || origWord.charAt(i)== 'i' ||origWord.charAt(i)== 'e' ||origWord.charAt(i)== 'o' || origWord.charAt(i)== 'o'){
-                        //i is the position of the vowel
-                        String start = origWord.substring(0,i);
-//                      String mid = origWord.substring(i);
-                        String end = origWord.substring(i);
-                        //Translated word 
-                        String transWord = start + "ub" + end;
-                        System.out.println(origWord+" in ubbi dubbi is "+transWord);
-                        
-                        //Done looking so stop loop
-                        break;
-                                
+                    if (origWord.charAt(i) == 'a') {
+                        String transWord = origWord.replace("a", "uba");
+                        System.out.println("Translated: " + transWord);
+                    }
+                    if (origWord.charAt(i) == 'i') {
+                        String transWord = origWord.replace("i", "ubi");
+                        System.out.println("Translated: " + transWord);
+                    }
+                    if (origWord.charAt(i) == 'o') {
+                        String transWord = origWord.replace("o", "ubo");
+                        System.out.println("Translated: " + transWord);
+                    }
+                    if (origWord.charAt(i) == 'e') {
+                        String transWord = origWord.replace("e", "ube");
+                        System.out.println("Translated: " + transWord);
+                    }
+                    if (origWord.charAt(i) == 'u') {
+                        String transWord = origWord.replace("u", "ubu");
+                        System.out.println("Translated: " + transWord);
                     }
                 }
             }
-            
         }
-        
-
-
     }
 }
