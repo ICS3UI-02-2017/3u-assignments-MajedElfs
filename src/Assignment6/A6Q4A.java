6a53b739fa02b0189f7bfc9114f552f2e41d6652
hadim9637 <hadim9637@HRH-IC0027346.wrdsb.ca>
2018-04-09 11:14:38 -0400

    Working on question 4

--- src/Assignment6/A6Q4A.java
+++ src/Assignment6/A6Q4A.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author hadim9637
  */
-public class A6Q4 {
+public class A6Q4A {
 
     /**
      * @param args the command line arguments
@@ -22,29 +22,31 @@ public class A6Q4 {
         System.out.println("Enter ten numbers to arrange:");
         //Making a ten integer array
         int[] nums = new int[10];
-
+        
         //Storing ten numbers
         for (int i = 0; i < nums.length; i++) {
             nums[i] = input.nextInt();
         }
+        
         for (int i = 0; i < nums.length; i++) {
-            //If bigger number is first, swap numbers
-            if (i + 1 >= nums.length) {
-                    i = 0;
-                }
             
-            if (nums[i] > nums[i + 1]) {
-                //Making temporary storage int
+        for (int j = 1; j < nums.length; j++) {
+            
+            if (nums[i] > nums[j]){
                 int temp = nums[i];
-                nums[i] = nums[i + 1];
-                nums[i + 1] = temp;
-                
+                nums[i] = nums[j];
+                nums[j] = temp;  
             }
+            
         }
-        System.out.println("Arranged numbers from lowest to highest: "
-                + nums[0] + ", " + nums[1] + ", " + nums[3] + ", " + nums[4] + ", "
+        }
+        
+System.out.println("Arranged numbers from lowest to highest: "
+                + nums[0] + ", " + nums[1] + ", "+nums[2]+", " + nums[3] + ", " + nums[4] + ", "
                 + nums[5] + ", " + nums[6] + ", " + nums[7] + ", " + nums[8] + ", " + nums[9]);
+        
 
 
-    }
+    
+}
 }
