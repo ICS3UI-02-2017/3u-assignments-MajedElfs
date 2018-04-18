a02504b3bfccd426a347155e25771ebc6a7210c4
MajedElfs <36414117+MajedElfs@users.noreply.github.com>
2018-04-18 18:30:20 -0400

    Update A6Q7.java

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Assignment6;
+
 
 import java.util.Scanner;
 
@@ -20,10 +20,30 @@ public class A6Q7 {
         //Making a scanner
         Scanner input = new Scanner(System.in);
         //Making for loop to 1000
-        int[] nums= new int [1000];
-        for (int i = 1; i < nums.length; i++) {
-            int p = 2;
-            
+        int[] nums = new int[1001];
+        for (int i = 2; i < nums.length; i++) {
+            nums[i] = i;
+
+        }
+
+        // p represents amount of multiples we iterate through.
+        for (int p = 2; p < 100; p++) {
+
+            //the numbers 
+            for (int i = 2; i < nums.length; i++) {
+
+                int temp = i * p;
+                if (temp <= 1000) {
+                    nums[temp] = 0;
+                }
+            }
+
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                System.out.println(nums[j]);
+            }
 
         }
 
