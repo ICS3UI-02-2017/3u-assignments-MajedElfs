4a6d8afef1b75b632e67dcb841cfa8ce6545abe0
hadim9637 <hadim9637@HRH-IC0027346.wrdsb.ca>
2018-05-01 11:20:36 -0400

    Assignment 7 question 5

--- src/Assignment7/A7Q8.java
+++ src/Assignment7/A7Q8.java
@@ -1,19 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package Assignment7;
-
 import java.util.Scanner;
 
 /**
  *
  * @author hadim9637
  */
-public class A7Q8 {
+public class A7Q8{
     
-    public static void allDigitsOdd(int digit){
+    public static Boolean allDigitsOdd(int digit){
+
+        int answer = digit%10;
         
+        while(answer == 1 || answer == 3 || answer == 5 || answer == 7 || answer == 9){
+          
+            //as long as it's not down to last number
+            if(digit > 10){
+                //chops number off
+                digit = digit/10;
+                //takes the NEW last number
+                answer = digit%10;
+            }else{
+               return true;
+            }
+            
+            
+            
+        }
+        return false;
     }
 
     /**
@@ -26,8 +38,15 @@ public class A7Q8 {
         System.out.println("Enter digit:");
         //Storing number into an integer
         int digit = input.nextInt();
+        
+        
+       
         //Putting the integer into the method
         allDigitsOdd(digit);
+        
+        System.out.println(allDigitsOdd(digit));
+        
+        
         
     }
 }
