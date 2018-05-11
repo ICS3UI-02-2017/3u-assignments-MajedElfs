e4a578e0963e4dea2fb037af39b225ac984a5039
hadim9637 <hadim9637@HRH-IC0027346.wrdsb.ca>
2018-05-11 11:18:51 -0400

    Drawing Period 4

--- src/Assignment8/A8Q1.java
+++ src/Assignment8/A8Q1.java
@@ -25,38 +25,35 @@ public class A8Q1 extends JComponent implements ActionListener {
     // Height and Width of our game
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
-
     //Title of the window
     String title = "Face";
-
     // sets the framerate and delay for our game
     // this calculates the number of milliseconds per frame
     // you just need to select an approproate framerate
     int desiredFPS = 60;
     int desiredTime = Math.round((1000 / desiredFPS));
-    
     // timer used to run the game loop
     // this is what keeps our time running smoothly :)
     Timer gameTimer;
-
     // YOUR GAME VARIABLES WOULD GO HERE
     //Creating a new colour
     Color skin = new Color(247, 215, 128);
     Color purple = new Color(182, 130, 255);
     Color skinShade = new Color(232, 189, 62);
-    Color darkPurple = new Color (149, 2, 247);
-    Color sky = new Color (43, 187, 255);
+    Color darkPurple = new Color(149, 2, 247);
+    Color sky = new Color(43, 187, 255);
     Color grass = new Color(28, 168, 58);
     Color eyePurple = new Color(74, 0, 114);
-    
-    int leafY = 100;
+    int leafY[] = {100, 300};
+    int leafYStart[] = {100, 300, 550};
+    int leafX[] = {100, 400};
+    int angle = 0;
+    int amplitude = 100;
 
     // GAME VARIABLES END HERE    
-
-    
     // Constructor to create the Frame and place the panel in
     // You will learn more about this in Grade 12 :)
-    public A8Q1(){
+    public A8Q1() {
         // creates a windows to show my game
         JFrame frame = new JFrame(title);
 
@@ -78,8 +75,8 @@ public class A8Q1 extends JComponent implements ActionListener {
         this.addMouseMotionListener(m);
         this.addMouseWheelListener(m);
         this.addMouseListener(m);
-        
-        gameTimer = new Timer(desiredTime,this);
+
+        gameTimer = new Timer(desiredTime, this);
         gameTimer.setRepeats(true);
         gameTimer.start();
     }
@@ -89,8 +86,8 @@ public class A8Q1 extends JComponent implements ActionListener {
     // NOTE: This is already double buffered!(helps with framerate/speed)
     @Override
     public void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-        
+        Graphics2D g2d = (Graphics2D) g;
+
         // always clear the screen first!
         g.clearRect(0, 0, WIDTH, HEIGHT);
 
@@ -100,6 +97,8 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.fillRect(0, 0, 800, 600);
         g.setColor(grass);
         g.fillRect(0, 500, 800, 100);
+        
+        g.fillRect(100, 500, 50, 100);
         //Making face structure
         g.setColor(skin);
         g.fillOval(200, 80, 350, 425);
@@ -112,11 +111,11 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.fillOval(305, 435, 100, 100);
         g.fillOval(370, 410, 100, 100);
         g.fillOval(350, 437, 100, 100);
-        
+
         g.setColor(Color.white);
         //Making eyes
-        int [] polygonX = {450, 475, 465};
-        int [] polygonY = {275, 275, 300};
+        int[] polygonX = {450, 475, 465};
+        int[] polygonY = {275, 275, 300};
         g.fillPolygon(polygonX, polygonY, 3);
         g.fillOval(425, 250, 75, 75);
         g.setColor(skin);
@@ -132,9 +131,9 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.fillOval(435, 260, 35, 35);
         g.setColor(eyePurple);
         g.fillOval(445, 270, 15, 15);
-        
-        
-        int [] polygonX2 = {75, 100, 90};
+
+
+        int[] polygonX2 = {75, 100, 90};
         g2d.translate(375, 0);
         g2d.scale(-1, 1);
         g.setColor(Color.white);
@@ -155,16 +154,16 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.fillOval(70, 270, 15, 15);
         g2d.scale(-1, 1);
         g2d.translate(-375, 0);
-        
+
         //Making ears
         g.setColor(skin);
-        int[]earX = {500,700, 475};
-        int[]earY ={260,260, 350};
+        int[] earX = {500, 700, 475};
+        int[] earY = {260, 260, 350};
         g.fillPolygon(earX, earY, 3);
-        int[]otherEarX = {250,50,275};
-        int[]otherEarY = {260,260,350};
+        int[] otherEarX = {250, 50, 275};
+        int[] otherEarY = {260, 260, 350};
         g.fillPolygon(otherEarX, otherEarY, 3);
-        
+
         //Making shading of neck
         g.setColor(skinShade);
         g.fillOval(335, 425, 100, 100);
@@ -174,18 +173,18 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.setColor(skinShade);
         g.fillOval(200, 300, 100, 100);
         g.fillOval(460, 300, 100, 100);
-        
+
         //Shading
         g.setColor(skinShade);
-        int[]hoodShadeX = {380,430,460};
-        int[]hoodShadeY = {225,160,170};
+        int[] hoodShadeX = {380, 430, 460};
+        int[] hoodShadeY = {225, 160, 170};
         g.fillPolygon(hoodShadeX, hoodShadeY, 3);
-        
+
         //Making hood
         g.setColor(purple);
         g.fillOval(280, 60, 200, 100);
-        int[] hoodFrontX = {280,480,380};
-        int[] hoodFrontY = {100,100,225};
+        int[] hoodFrontX = {280, 480, 380};
+        int[] hoodFrontY = {100, 100, 225};
         g.fillPolygon(hoodFrontX, hoodFrontY, 3);
         g.fillOval(450, 100, 75, 100);
         g.fillOval(420, 125, 100, 50);
@@ -198,10 +197,10 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.fillOval(485, 325, 75, 100);
         g.fillOval(475, 350, 75, 100);
         g.fillOval(450, 375, 75, 100);
-	g.fillOval(425, 400, 75, 100);	
-        g.fillOval(400, 425, 75, 100);	
+        g.fillOval(425, 400, 75, 100);
+        g.fillOval(400, 425, 75, 100);
         g.fillOval(375, 475, 75, 100);
-	g.fillOval(340, 475, 100, 100);	
+        g.fillOval(340, 475, 100, 100);
         g.fillOval(320, 450, 75, 100);
         g.fillOval(300, 425, 75, 100);
         g.fillOval(280, 425, 75, 100);
@@ -223,18 +222,18 @@ public class A8Q1 extends JComponent implements ActionListener {
         //Front hood design
         g.setColor(darkPurple);
         g.fillArc(330, 115, 100, 100, 35, 125);
-        
-        
+
+
         //Making nose
         g.setColor(skinShade);
-        int[]noseX = {380,370,390};
-        int[]noseY = {300,325,325};
+        int[] noseX = {380, 370, 390};
+        int[] noseY = {300, 325, 325};
         g.fillPolygon(noseX, noseY, 3);
-        
+
         //Making mouth
         g.setColor(Color.BLACK);
         g.drawLine(345, 380, 425, 380);
-        
+
         //Making body
         g.setColor(purple);
         g.fillOval(150, 500, 500, 500);
@@ -243,12 +242,14 @@ public class A8Q1 extends JComponent implements ActionListener {
         g.fillOval(205, 550, 100, 100);
         g.fillOval(425, 500, 100, 100);
         g.setColor(darkPurple);
-        int[]bodyX = {390,320,450};
-        int[]bodyY = {500,600,600};
+        int[] bodyX = {390, 320, 450};
+        int[] bodyY = {500, 600, 600};
         g.fillPolygon(bodyX, bodyY, 3);
-        
+
         g.setColor(Color.GREEN);
-        g.fillOval(100, leafY, 50, 50);
+        for (int i = 0; i < 2; i++) {
+            g.fillOval(leafX[i], leafY[i], 50, 50);
+        }
         // GAME DRAWING ENDS HERE
     }
 
@@ -256,16 +257,33 @@ public class A8Q1 extends JComponent implements ActionListener {
     // This is run before the game loop begins!
     public void preSetup() {
         // Any of your pre setup before the loop starts should go here
-
     }
 
     // The main game loop
     // In here is where all the logic for my game will go
     public void gameLoop() {
-        leafY = leafY + 5;
-        if(leafY > HEIGHT){
-            leafY = 0;
+        for (int i = 0; i < 100; i++) {
+            
+            
         }
+        for (int i = 0; i < 2; i++) {
+            leafX[i] = leafX[i] + 5;
+
+            if (leafX[i] > WIDTH) {
+                leafX[i] = 0;
+            }
+            int amount = (int) (amplitude * Math.sin(Math.toRadians(angle)));
+            leafY[i] = leafYStart[i] + amount;
+        }
+
+        angle = angle + 2;
+        if (angle == 360) {
+            angle = 0;
+        }
+        
+  
+        
+
     }
 
     // Used to implement any of the Mouse Actions
@@ -274,25 +292,21 @@ public class A8Q1 extends JComponent implements ActionListener {
         // if a mouse button has been pressed down
         @Override
         public void mousePressed(MouseEvent e) {
-
         }
 
         // if a mouse button has been released
         @Override
         public void mouseReleased(MouseEvent e) {
-
         }
 
         // if the scroll wheel has been moved
         @Override
         public void mouseWheelMoved(MouseWheelEvent e) {
-
         }
 
         // if the mouse has moved positions
         @Override
         public void mouseMoved(MouseEvent e) {
-
         }
     }
 
@@ -302,13 +316,11 @@ public class A8Q1 extends JComponent implements ActionListener {
         // if a key has been pressed down
         @Override
         public void keyPressed(KeyEvent e) {
-
         }
 
         // if a key has been released
         @Override
         public void keyReleased(KeyEvent e) {
-
         }
     }
 
@@ -327,4 +339,3 @@ public class A8Q1 extends JComponent implements ActionListener {
         A8Q1 game = new A8Q1();
     }
 }
-
