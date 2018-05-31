8085b75946f9f8730eb10e2be894f9762c9d400e
hadim9637 <hadim9637@HRH-IC0027346.wrdsb.ca>
2018-05-31 11:18:59 -0400

    Making game: Day 7

--- src/FinalProject/FinalProject.java
+++ src/FinalProject/FinalProject.java
@@ -37,27 +37,39 @@ public class FinalProject extends JComponent implements ActionListener {
     Timer gameTimer;
     // YOUR GAME VARIABLES WOULD GO HERE
     Color grass = new Color(23, 183, 31);
+    //Making Character and enemies
     Rectangle mainChar = new Rectangle(400, 350, 50, 50);
     Rectangle enemy = new Rectangle(500, 10, 50, 50);
-    Rectangle eliteEnemy = new Rectangle(500, -50, 75, 75);
-    Rectangle sword = new Rectangle (mainChar.x+50,mainChar.y+15,50,10);
-    
+    Rectangle enemy2 = new Rectangle(680, -50, 50, 50);
+    Rectangle eliteEnemy = new Rectangle(250, -100, 75, 75);
+    Rectangle eliteEnemy2 = new Rectangle(50, -100, 75, 75);
+    Rectangle eliteEnemy3 = new Rectangle(700, -100, 75, 75);
+    Rectangle boulder = new Rectangle(-100, 350, 40, 40);
+    Rectangle sword = new Rectangle(mainChar.x + 50, mainChar.y + 15, 50, 10);
     //Control variables
     boolean charRight = false;
     boolean charLeft = false;
     //Speed of Character
     int charSpeed = 5;
     //Monster's fall speed
-    int mobFallSpeed = 2;
+    int mobFallSpeed = 4;
     int originalPosX = 500;
     int originalPosY = 350;
     boolean movingRight = true;
     int stopPosLeft = enemy.x - 100;
     int stopPosRight = enemy.x + 100;
+    int stopPosLeft2 = enemy2.x - 100;
+    int stopPosRight2 = enemy2.x + 100;
+    int eStopPosLeft2 = eliteEnemy2.x - 100;
+    int eStopPosRight2 = eliteEnemy2.x + 100;
+    int eStopPosLeft = eliteEnemy.x - 200;
+    int eStopPosRight = eliteEnemy.x + 200;
+    int eStopPosLeft3 = eliteEnemy3.x - 200;
+    int eStopPosRight3 = eliteEnemy3.x + 200;
     //Score and Health
     int health = 3;
     int score = 0;
-    Font BiggerFont = new Font("arial",Font.BOLD,36);
+    Font BiggerFont = new Font("arial", Font.BOLD, 36);
     //Getting hit (flashing)
     long flashUntil = System.currentTimeMillis();
     int flashDelay = 50;
@@ -68,14 +80,14 @@ public class FinalProject extends JComponent implements ActionListener {
     //Hitting
     boolean hitting = false;
     boolean swordApperance = false;
-    int swordPosLeft = mainChar.x-50;
+    int swordPosLeft = mainChar.x - 50;
     //Jumping
     boolean jumping = false;
     boolean onGround = true;
     //Adding gravity
     int gravity = 1;
     int velocity = 0;
-
+    boolean boulderGoingRight = true;
 
     // GAME VARIABLES END HERE    
     // Constructor to create the Frame and place the panel in
@@ -122,44 +134,49 @@ public class FinalProject extends JComponent implements ActionListener {
         g.setColor(Color.GRAY);
         g.fillRect(0, 400, WIDTH, 650);
 
-        
+
         g.setColor(Color.WHITE);
         g.fillRect(mainChar.x, mainChar.y, mainChar.width, mainChar.height);
         //If hitting, make sword appear
-        if (swordApperance && !facingLeft){
+        if (swordApperance && !facingLeft) {
             g.setColor(Color.YELLOW);
             //Sword
-           g.fillRect(mainChar.x+50,mainChar.y+15, sword.width, sword.height); 
-           sword.x=mainChar.x+50;
-           sword.y = mainChar.y+15;
-                  
+            g.fillRect(mainChar.x + 50, mainChar.y + 15, sword.width, sword.height);
+            sword.x = mainChar.x + 50;
+            sword.y = mainChar.y + 15;
         }
-        if (swordApperance && facingLeft){
+        if (swordApperance && facingLeft) {
             g.setColor(Color.YELLOW);
             //Sword
-           g.fillRect(mainChar.x-50,mainChar.y+15, sword.width, sword.height);
-            sword.x = mainChar.x-50;
-            sword.y = mainChar.y+15;
+            g.fillRect(mainChar.x - 50, mainChar.y + 15, sword.width, sword.height);
+            sword.x = mainChar.x - 50;
+            sword.y = mainChar.y + 15;
         }
-        
-     
+
+        //Drawing enemies
         g.setColor(Color.RED);
         g.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
         g.fillRect(eliteEnemy.x, eliteEnemy.y, eliteEnemy.width, eliteEnemy.height);
+        g.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height);
+        g.fillRect(eliteEnemy2.x, eliteEnemy2.y, eliteEnemy2.width, eliteEnemy2.height);
+        g.fillRect(eliteEnemy3.x, eliteEnemy3.y, eliteEnemy3.width, eliteEnemy3.height);
+        g.setColor(Color.GRAY);
+        g.fillRect(boulder.x, boulder.y, boulder.width, boulder.height);
+        g.setColor(Color.GREEN);
         g.setFont(BiggerFont);
-        g.drawString(""+health, 50, 50);
-        g.drawString("Points: "+score, 800, 50);
-        if (flash){
+        g.drawString("" + health, 50, 50);
+        g.drawString("Points: " + score, 800, 50);
+        if (flash) {
             g.setColor(Color.RED);
             g.fillRect(0, 0, WIDTH, HEIGHT);
         }
-        if (health <= 0){
+        if (health <= 0) {
             g.clearRect(0, 0, WIDTH, HEIGHT);
             g.setFont(BiggerFont);
             g.drawString(gameOver, 400, 350);
         }
-        
-        
+
+
 
 
         // GAME DRAWING ENDS HERE
@@ -179,7 +196,8 @@ public class FinalProject extends JComponent implements ActionListener {
         sword();
         healthBar();
         jump();
-        
+        boulder();
+
         collisionWithGround();
     }
 
@@ -205,8 +223,10 @@ public class FinalProject extends JComponent implements ActionListener {
         //Makes the enemy roam right and left
         if (movingRight) {
             enemy.x++;
+
         } else {
             enemy.x--;
+
         }
         if (enemy.x == stopPosRight) {
             movingRight = false;
@@ -218,38 +238,176 @@ public class FinalProject extends JComponent implements ActionListener {
         } else if (enemy.x < 0) {
             enemy.x = 0;
         }
-        if (enemy.x <= 0){
+        if (enemy.x <= 0) {
             movingRight = true;
-        }else if (enemy.x >= WIDTH){
+        } else if (enemy.x >= WIDTH) {
             movingRight = false;
         }
-        if (score == 1000){
-            if (eliteEnemy.y < 350) {
-            eliteEnemy.y = eliteEnemy.y + mobFallSpeed;
+        //When player reaches 1000 points, an elite enemy spawns
+
+        if (score >= 1000) {
+            if (eliteEnemy.y < 325) {
+                eliteEnemy.y = eliteEnemy.y + mobFallSpeed;
             }
-            
+
+            //Elite enemy movements
         }
-        
+        if (eliteEnemy.x == eStopPosRight) {
+            movingRight = false;
+
+        } else if (eliteEnemy.x == eStopPosLeft) {
+            movingRight = true;
+        }
+        if (movingRight) {
+            eliteEnemy.x++;
+        } else {
+            eliteEnemy.x--;
+        }
+        //Enemy 2 movements
+        if (score >= 500) {
+            if (enemy2.y < 350) {
+                enemy2.y = enemy2.y + mobFallSpeed;
+            }
+
+        }
+        if (movingRight) {
+            enemy2.x++;
+        } else {
+            enemy2.x--;
+        }
+        if (enemy2.x > WIDTH) {
+            enemy2.x = WIDTH - enemy2.width;
+        } else if (enemy2.x < 0) {
+            enemy2.x = 0;
+        }
+        if (enemy2.x <= 0) {
+            movingRight = true;
+        } else if (enemy2.x >= WIDTH) {
+            movingRight = false;
+        }
+
+
+        if (enemy2.x == stopPosRight) {
+            movingRight = false;
+        } else if (enemy2.x == stopPosLeft) {
+            movingRight = true;
+        }
+        if (enemy2.x > WIDTH) {
+            enemy2.x = WIDTH - enemy2.width;
+        } else if (enemy2.x < 0) {
+            enemy2.x = 0;
+        }
+        if (enemy2.x <= 0) {
+            movingRight = true;
+        } else if (enemy2.x >= WIDTH) {
+            movingRight = false;
+        }
+        //When over 4000 points, spawn 2 more enemies (max amount of enemies on screen now.. 5)
+        if (score >= 4000) {
+            if (eliteEnemy2.y < 325) {
+                eliteEnemy2.y = eliteEnemy2.y + mobFallSpeed;
+            }
+            if (eliteEnemy3.y < 325) {
+                eliteEnemy3.y = eliteEnemy3.y + mobFallSpeed;
+            }
+        }
+        if (movingRight) {
+            eliteEnemy2.x++;
+            eliteEnemy3.x++;
+        } else {
+            eliteEnemy2.x--;
+            eliteEnemy3.x--;
+        }
+        if (eliteEnemy2.x > WIDTH) {
+            eliteEnemy2.x = WIDTH - eliteEnemy2.width;
+        } else if (eliteEnemy2.x < 0) {
+            eliteEnemy2.x = 0;
+        }
+        if (eliteEnemy2.x <= 0) {
+            movingRight = true;
+        } else if (eliteEnemy2.x >= WIDTH) {
+            movingRight = false;
+        }
+        if (eliteEnemy2.x == stopPosRight) {
+            movingRight = false;
+        } else if (eliteEnemy2.x == stopPosLeft) {
+            movingRight = true;
+        }
+        if (eliteEnemy2.x > WIDTH) {
+            eliteEnemy2.x = WIDTH - eliteEnemy2.width;
+        } else if (eliteEnemy2.x < 0) {
+            eliteEnemy2.x = 0;
+        }
+        if (eliteEnemy2.x <= 0) {
+            movingRight = true;
+        } else if (eliteEnemy2.x >= WIDTH) {
+            movingRight = false;
+        }
+
+        if (eliteEnemy3.x > WIDTH) {
+            eliteEnemy3.x = WIDTH - eliteEnemy3.width;
+        } else if (eliteEnemy3.x < 0) {
+            eliteEnemy3.x = 0;
+        }
+        if (eliteEnemy3.x <= 0) {
+            movingRight = true;
+        } else if (eliteEnemy3.x >= WIDTH) {
+            movingRight = false;
+        }
+        if (eliteEnemy3.x == stopPosRight) {
+            movingRight = false;
+        } else if (eliteEnemy3.x == stopPosLeft) {
+            movingRight = true;
+        }
+        if (eliteEnemy3.x > WIDTH) {
+            eliteEnemy3.x = WIDTH - eliteEnemy3.width;
+        } else if (eliteEnemy3.x < 0) {
+            eliteEnemy3.x = 0;
+        }
+        if (eliteEnemy3.x <= 0) {
+            movingRight = true;
+        } else if (eliteEnemy3.x >= WIDTH) {
+            movingRight = false;
+        }
 
     }
 
     private void sword() {
-        
-        if (hitting){
+
+        if (hitting) {
             //When sword touches enemy, it "dies" and another spawns on top of screen.
-            if (swordApperance){
-                if (sword.intersects(enemy)){
-                    enemy.x = (int)(Math.random() * 1000) + 0;
+            if (swordApperance) {
+                if (sword.intersects(enemy)) {
+                    enemy.x = (int) (Math.random() * 1000) + 0;
                     enemy.y = 0;
                     score = score + 100;
                 }
-                
+                if (sword.intersects(enemy2)) {
+                    enemy2.x = (int) (Math.random() * 1000) + 0;
+                    enemy2.y = 0;
+                    score = score + 100;
+                }
+                if (sword.intersects(eliteEnemy)) {
+                    score = score + 250;
+                    eliteEnemy.y = -100;
+                }
+                if (sword.intersects(eliteEnemy2)) {
+                    score = score + 250;
+                    eliteEnemy2.y = -100;
+                }
+                if (sword.intersects(eliteEnemy3)) {
+                    score = score + 250;
+                    eliteEnemy3.y = -100;
+                }
+
+
+
             }
-            
-            
-        
+
+
+
         }
-        
+
     }
 
     private void healthBar() {
@@ -257,45 +415,70 @@ public class FinalProject extends JComponent implements ActionListener {
         if (mainChar.intersects(enemy)) {
             health--;
             //If enemy is on the right of the main char and is touched, bounce back
-            if (enemy.x>mainChar.x){
+            if (enemy.x > mainChar.x) {
                 mainChar.x = mainChar.x - 50;
             }//If enemy is on the left of the main char and is touched, bounce back
-            else if (enemy.x<mainChar.x){
-               mainChar.x = mainChar.x + 50; 
+            else if (enemy.x < mainChar.x) {
+                mainChar.x = mainChar.x + 50;
+
             }
-            
             flash = true;
             flashUntil = System.currentTimeMillis() + flashDelay;
         }
-        
-        if(System.currentTimeMillis() > flashUntil){
+        if (mainChar.intersects(eliteEnemy)) {
+            health--;
+            //If enemy is on the right of the main char and is touched, bounce back
+            if (eliteEnemy.x > mainChar.x) {
+                mainChar.x = mainChar.x - 50;
+            }//If enemy is on the left of the main char and is touched, bounce back
+            else if (eliteEnemy.x < mainChar.x) {
+                mainChar.x = mainChar.x + 50;
+            }
+
+            flash = true;
+            flashUntil = System.currentTimeMillis() + flashDelay;
+        }
+        if (mainChar.intersects(enemy2)) {
+            health--;
+            //If enemy is on the right of the main char and is touched, bounce back
+            if (enemy2.x > mainChar.x) {
+                mainChar.x = mainChar.x - 50;
+            }//If enemy is on the left of the main char and is touched, bounce back
+            else if (enemy2.x < mainChar.x) {
+                mainChar.x = mainChar.x + 50;
+            }
+            flash = true;
+            flashUntil = System.currentTimeMillis() + flashDelay;
+        }
+
+        if (System.currentTimeMillis() > flashUntil) {
             flash = false;
         }
-       
+
+
     }
 
     private void jump() {
         velocity = velocity + gravity;
-        if (jumping && onGround){
-            velocity= -15;
-            onGround=false;
+        if (jumping && onGround) {
+            velocity = -15;
+            onGround = false;
         }
         mainChar.y = mainChar.y + velocity;
-        if (mainChar.y >=350){
-            onGround=true;
+        if (mainChar.y >= 350) {
+            onGround = true;
         }
-        
+
     }
 
-    
-            
-    
-
     private void collisionWithGround() {
-        if (mainChar.y>350){
+        if (mainChar.y > 350) {
             mainChar.y = 350;
         }
-        
+
+    }
+
+    private void boulder() {
     }
 
     // Used to implement any of the Mouse Actions
@@ -335,20 +518,20 @@ public class FinalProject extends JComponent implements ActionListener {
             } else if (keyCode == KeyEvent.VK_LEFT) {
                 charLeft = true;
                 facingLeft = true;
-                
+
             }
-            if (keyCode == KeyEvent.VK_UP){
+            if (keyCode == KeyEvent.VK_UP) {
                 jumping = true;
-   
+
             }
-            if (keyCode ==KeyEvent.VK_SPACE){
+            if (keyCode == KeyEvent.VK_SPACE) {
                 hitting = true;
                 swordApperance = true;
-            
-            
-            
-            
-            
+
+
+
+
+
             }
         }
 
@@ -363,16 +546,16 @@ public class FinalProject extends JComponent implements ActionListener {
                 charLeft = false;
                 facingLeft = true;
             }
-            
-            if (keyCode ==KeyEvent.VK_SPACE){
+
+            if (keyCode == KeyEvent.VK_SPACE) {
                 hitting = true;
                 swordApperance = false;
             }
-             if (keyCode == KeyEvent.VK_UP){
+            if (keyCode == KeyEvent.VK_UP) {
                 jumping = false;
-               
+
             }
-            
+
 
         }
     }
